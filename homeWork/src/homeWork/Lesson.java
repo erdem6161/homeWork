03762e3487f3f40500d55274b61b398f2b03d3d5
Erdem Demircioglu <64333517+erdem6161@users.noreply.github.com>
2021-04-25 23:35:43 +0300

    Example

---
+++ homeWork/src/homeWork/Lesson.java
@@ -0,0 +1,26 @@
+package homeWork;
+
+public class Lesson {
+
+	public Lesson() {
+		
+		
+	}
+	
+	public Lesson(String name,String detail,float percent ) {
+		
+		this.name=name;
+		this.detail=detail;
+		this.percent=percent;
+		
+	}
+	
+	
+	String name;
+	String detail;
+	float percent;
+	
+	
+	
+	
+}
---
+++ homeWork/src/homeWork/Main.java
@@ -0,0 +1,14 @@
+package homeWork;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Lesson lesson1 = new Lesson("Java Kamp","Kurs Programı",100);
+		Lesson lesson2 = new Lesson("C#","Kurs Programı",90);
+
+	}
+
+}
